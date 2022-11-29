c7c5bcf46417a3c458c397d3ad7514ee157113fe
atorov <veselin.lkp@gmail.com>
2022-11-29 06:07:04 +0200

    v6.1.0 -- updated project deps, updated webpack config  and all other related changes

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ declare const APP_NAME: string
 declare const APP_VERSION: string
 declare const MODE: string
 declare const NODE_ENV: string
+declare const BUILD_ENV: string
 
 const appRootElement: HTMLElement | null = document.querySelector('#app-root')
 const buildDate = appRootElement?.dataset.buildDate
@@ -15,8 +16,9 @@ console.log('::: APP_NAME:', APP_NAME)
 console.log('::: APP_VERSION:', APP_VERSION)
 console.log('::: MODE:', MODE)
 console.log('::: NODE_ENV:', NODE_ENV)
-console.log('::: Build Date:', buildDate)
 console.log('::: process.env.NODE_ENV (NODE_ENV):', process.env.NODE_ENV)
+console.log('::: BUILD_ENV:', BUILD_ENV)
+console.log('::: Build Date:', buildDate)
 console.log('::: ::: :::')
 
 const container = document.querySelector('#app-root') as Element
--- webpack.config.js
+++ webpack.config.js
@@ -7,66 +7,88 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const pkg = require('./package.json')
 
-const APP_NAME = pkg.name
-const APP_VERSION = pkg.version
+module.exports = (env) => {
+    const APP_NAME = pkg.name
+    const APP_VERSION = pkg.version
 
-const HOST = process.env.HOST || '0.0.0.0'
-const PORT = process.env.PORT || 8080
+    const HOST = env.HOST || '0.0.0.0'
+    const PORT = env.PORT || 8080
+    const BUILD_ENV = env.BUILD_ENV || 'production'
 
-const NODE_MODULES = path.resolve(__dirname, 'node_modules')
-const EXTERNALS = path.resolve(__dirname, 'externals')
-const STORAGE = path.resolve(__dirname, '__storage__')
-const EXCLUDE_DEFAULT = [NODE_MODULES, EXTERNALS, STORAGE]
+    const { NODE_ENV } = process.env
+    const MODE = NODE_ENV !== 'development' ? 'production' : 'development'
 
-const SRC = path.resolve(__dirname, 'src')
-// const PUBLIC = path.resolve(__dirname, 'build/public')
-// const DIST = path.resolve(__dirname, 'build/public/ai-art-datasets')
-const DIST = path.resolve(__dirname, 'build/public')
+    const NODE_MODULES = path.resolve(__dirname, 'node_modules')
+    const EXTERNALS = path.resolve(__dirname, 'externals')
+    const STORAGE = path.resolve(__dirname, '__storage__')
+    const EXCLUDE_DEFAULT = [NODE_MODULES, EXTERNALS, STORAGE]
 
-const { NODE_ENV } = process.env
-const MODE = NODE_ENV !== 'development' ? 'production' : 'development'
+    const SRC = path.resolve(__dirname, 'src')
+    // const PUBLIC = path.resolve(__dirname, 'build/public')
+    // const DIST = path.resolve(__dirname, 'build/public/ai-art-datasets')
+    const DIST = path.resolve(__dirname, 'build/public')
 
-const config = {
-    mode: MODE,
+    const config = {
+        mode: MODE,
 
-    // https://github.com/webpack/webpack-dev-server/issues/2758
-    // Solved for webpack-dev-server v4
-    // target: MODE === 'development' ? 'web' : 'browserslist',
+        // https://github.com/webpack/webpack-dev-server/issues/2758
+        // Solved for webpack-dev-server v4
+        // target: MODE === 'development' ? 'web' : 'browserslist',
 
-    resolve: {
-        extensions: ['.ts', '.tsx', '.json', '.js'],
-    },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.json', '.js'],
+        },
 
-    entry: [
-        'core-js/stable',
-        'regenerator-runtime/runtime',
-        'whatwg-fetch',
-        `${SRC}`,
-    ],
+        entry: [
+            'core-js/stable',
+            'regenerator-runtime/runtime',
+            'whatwg-fetch',
+            `${SRC}`,
+        ],
 
-    output: {
-        path: DIST,
-        // publicPath: '/ai-art-datasets/',
-        publicPath: '/',
-    },
+        output: {
+            path: DIST,
+            // publicPath: '/ai-art-datasets/',
+            publicPath: '/',
+        },
 
-    devtool: 'source-map',
+        devtool: 'source-map',
 
-    performance: {
-        maxEntrypointSize: MODE === 'production' ? 1000000 : 5000000,
-        maxAssetSize: MODE === 'production' ? 1000000 : 5000000,
-    },
+        performance: {
+            maxEntrypointSize: MODE === 'production' ? 1000000 : 5000000,
+            maxAssetSize: MODE === 'production' ? 1000000 : 5000000,
+        },
 
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                include: SRC,
-                exclude: EXCLUDE_DEFAULT,
-                use: (() => {
-                    if (MODE === 'development') {
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/,
+                    include: SRC,
+                    exclude: EXCLUDE_DEFAULT,
+                    use: (() => {
+                        if (MODE === 'development') {
+                            return [
+                                'style-loader',
+                                'css-loader',
+                                {
+                                    loader: 'postcss-loader',
+                                    options: {
+                                        postcssOptions: {
+                                            plugins: [
+                                                [
+                                                    'postcss-preset-env',
+                                                    { /* Options */ },
+                                                ],
+                                            ],
+                                        },
+                                    },
+                                },
+                                'sass-loader',
+                            ]
+                        }
+
                         return [
-                            'style-loader',
+                            MiniCssExtractPlugin.loader,
                             'css-loader',
                             {
                                 loader: 'postcss-loader',
@@ -83,181 +105,163 @@ const config = {
                             },
                             'sass-loader',
                         ]
-                    }
-
-                    return [
-                        MiniCssExtractPlugin.loader,
-                        'css-loader',
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                postcssOptions: {
-                                    plugins: [
-                                        [
-                                            'postcss-preset-env',
-                                            { /* Options */ },
-                                        ],
-                                    ],
-                                },
-                            },
+                    })(),
+                },
+                {
+                    test: /\.worker\.ts$/,
+                    include: SRC,
+                    exclude: EXCLUDE_DEFAULT,
+                    use: {
+                        loader: 'worker-loader',
+                        options: {
+                            filename: '[name].[contenthash:4].js',
                         },
-                        'sass-loader',
-                    ]
-                })(),
-            },
-            {
-                test: /\.worker\.ts$/,
-                include: SRC,
-                exclude: EXCLUDE_DEFAULT,
-                use: {
-                    loader: 'worker-loader',
-                    options: {
-                        filename: '[name].[contenthash:4].js',
                     },
                 },
-            },
-            // {
-            //     test: /\.jsx?$/,
-            //     include: SRC,
-            //     exclude: EXCLUDE_DEFAULT,
-            //     use: {
-            //         loader: 'babel-loader',
-            //     },
-            // },
-            // https://github.com/webpack/webpack/issues/11467
-            {
-                test: /\.m?js$/,
-                resolve: {
-                    fullySpecified: false, // disable the behavior
-                },
-            },
-            {
-                test: /\.tsx?$/,
-                include: SRC,
-                exclude: EXCLUDE_DEFAULT,
-                use: {
-                    loader: 'ts-loader',
-                },
-            },
-        ],
-    },
-
-    plugins: [
-        new webpack.WatchIgnorePlugin({ paths: EXCLUDE_DEFAULT }),
-
-        new webpack.DefinePlugin({
-            APP_NAME: JSON.stringify(APP_NAME),
-            APP_VERSION: JSON.stringify(APP_VERSION),
-            MODE: JSON.stringify(MODE),
-            NODE_ENV: JSON.stringify(NODE_ENV),
-            // 'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
-            process: { env: { NODE_ENV: JSON.stringify(NODE_ENV) } },
-        }),
-
-        new HtmlWebpackPlugin({
-            filename: `${DIST}/index.html`,
-            template: `${SRC}/index.ejs`,
-        }),
-
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash:4].css',
-        }),
-
-        new CopyWebpackPlugin({
-            patterns: [
+                // {
+                //     test: /\.jsx?$/,
+                //     include: SRC,
+                //     exclude: EXCLUDE_DEFAULT,
+                //     use: {
+                //         loader: 'babel-loader',
+                //     },
+                // },
+                // https://github.com/webpack/webpack/issues/11467
                 {
-                    from: `${SRC}/assets/img/favicon.png`,
-                    to: `${DIST}/favicon.png`,
-                },
-                {
-                    from: `${SRC}/assets/img`,
-                    to: `${DIST}/img`,
-                    globOptions: {
-                        ignore: ['.DS_Store'],
+                    test: /\.m?js$/,
+                    resolve: {
+                        fullySpecified: false, // disable the behavior
                     },
                 },
                 {
-                    from: `${SRC}/assets/local-db`,
-                    to: `${DIST}/local-db`,
-                    globOptions: {
-                        ignore: ['.DS_Store'],
+                    test: /\.tsx?$/,
+                    include: SRC,
+                    exclude: EXCLUDE_DEFAULT,
+                    use: {
+                        loader: 'ts-loader',
                     },
                 },
+            ],
+        },
+
+        plugins: [
+            new webpack.WatchIgnorePlugin({ paths: EXCLUDE_DEFAULT }),
+
+            new webpack.DefinePlugin({
+                APP_NAME: JSON.stringify(APP_NAME),
+                APP_VERSION: JSON.stringify(APP_VERSION),
+                MODE: JSON.stringify(MODE),
+                NODE_ENV: JSON.stringify(NODE_ENV),
+                // 'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+                // process: { env: { NODE_ENV: JSON.stringify(NODE_ENV) } }, <-- This could cause big troubles with styled-components and other libs!
+                BUILD_ENV: JSON.stringify(BUILD_ENV),
+            }),
+
+            new HtmlWebpackPlugin({
+                filename: `${DIST}/index.html`,
+                template: `${SRC}/index.ejs`,
+            }),
+
+            new MiniCssExtractPlugin({
+                filename: '[name].[contenthash:4].css',
+            }),
+
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: `${SRC}/assets/img/favicon.png`,
+                        to: `${DIST}/favicon.png`,
+                    },
+                    {
+                        from: `${SRC}/assets/img`,
+                        to: `${DIST}/img`,
+                        globOptions: {
+                            ignore: ['.DS_Store'],
+                        },
+                    },
+                    {
+                        from: `${SRC}/assets/local-db`,
+                        to: `${DIST}/local-db`,
+                        globOptions: {
+                            ignore: ['.DS_Store'],
+                        },
+                    },
                 // {
                 //     from: `${SRC}/assets/surge.sh`,
                 //     to: `${PUBLIC}/`,
                 // },
-            ],
-        }),
-    ],
-}
-
-// Production mode only settings -----------------------------------------------
-if (MODE === 'production') {
-    config.output.chunkFilename = '[name].[chunkhash:4].js'
-    config.output.filename = '[name].[chunkhash:4].js'
-
-    config.optimization = {
-        splitChunks: {
-            chunks: 'initial',
-        },
-        runtimeChunk: {
-            name: 'manifest',
-        },
+                ],
+            }),
+        ],
     }
-}
 
-// Development mode only settings ----------------------------------------------
-if (MODE === 'development') {
-    config.devServer = {
-        host: HOST,
-        port: PORT,
-        client: {
-            logging: 'info',
-            overlay: {
-                errors: true,
-                warnings: true,
+    // Production mode only settings -----------------------------------------------
+    if (MODE === 'production') {
+        config.output.chunkFilename = '[name].[chunkhash:4].js'
+        config.output.filename = '[name].[chunkhash:4].js'
+
+        config.optimization = {
+            splitChunks: {
+                chunks: 'initial',
             },
+            runtimeChunk: {
+                name: 'manifest',
+            },
+        }
+    }
+
+    // Development mode only settings ----------------------------------------------
+    if (MODE === 'development') {
+        config.devServer = {
+            host: HOST,
+            port: PORT,
+            client: {
+                logging: 'info',
+                overlay: {
+                    errors: true,
+                    warnings: true,
+                },
             // progress: true,
-        },
-        devMiddleware: {
+            },
+            devMiddleware: {
             // publicPath: '/ai-art-datasets/',
             // publicPath: '/',
             // stats: 'errors-only',
-            stats: 'minimal',
-        },
-        server: 'https',
-        // static: {
-        // directory: path.resolve(__dirname, "static"),
-        // staticOptions: {},
-        //
-        // Don't be confused with `devMiddleware.publicPath`, it is `publicPath` for static directory
-        // Can be:
-        // publicPath: ['/static-public-path-one/', '/static-public-path-two/'],
-        // publicPath: '/ai-art-datasets/',
-        // publicPath: '/',
-        //
-        // Can be:
-        // serveIndex: {} (options for the `serveIndex` option you can find https://github.com/expressjs/serve-index)
-        // serveIndex: true,
-        //
-        // Can be:
-        // watch: {} (options for the `watch` option you can find https://github.com/paulmillr/chokidar)
-        // watch: true,
-        // watch: {
-        //     aggregateTimeout: 300,
-        //     poll: 1000,
-        //     ignored: EXCLUDE_DEFAULT,
-        // },
-        // },
-        historyApiFallback: {
+                stats: 'minimal',
+            },
+            server: 'https',
+            // static: {
+            // directory: path.resolve(__dirname, "static"),
+            // staticOptions: {},
+            //
+            // Don't be confused with `devMiddleware.publicPath`, it is `publicPath` for static directory
+            // Can be:
+            // publicPath: ['/static-public-path-one/', '/static-public-path-two/'],
+            // publicPath: '/ai-art-datasets/',
+            // publicPath: '/',
+            //
+            // Can be:
+            // serveIndex: {} (options for the `serveIndex` option you can find https://github.com/expressjs/serve-index)
+            // serveIndex: true,
+            //
+            // Can be:
+            // watch: {} (options for the `watch` option you can find https://github.com/paulmillr/chokidar)
+            // watch: true,
+            // watch: {
+            //     aggregateTimeout: 300,
+            //     poll: 1000,
+            //     ignored: EXCLUDE_DEFAULT,
+            // },
+            // },
+            historyApiFallback: {
             // index: '/ai-art-datasets/index.html',
-            index: '/index.html',
-        },
-        allowedHosts: 'all',
+                index: '/index.html',
+            },
+            allowedHosts: 'all',
         // liveReload: true,
         // hot: false,
+        }
     }
-}
 
-module.exports = config
+    return config
+}
